85365bf4c5fb5ca0b2cf20b02ce2a1a5d931e06c
KannanVS <kannansanthanam97@gmail.com>
2019-07-08 12:23:21 +0200

    Trainer

--- view/homepage_view.php
+++ view/homepage_view.php
@@ -1,5 +1,17 @@
-<?php require_once("header.php"); ?>
+<?php require_once("header.php"); 
+
+  $trainer_id = $_SESSION["user"]["person_id"];
+  $is_trainer = $_SESSION["user"]["is_trainer"];
+
+
+ 
+    if ($_SESSION["user"]["is_trainer"] == 1) {
+        require_once("../model/TrainerHomeDao.php");
+    
+  }
+?>
 <div>
+
   <p> Welcome to Cefisi platform for training and Evaluation</p>
 
 </div>
